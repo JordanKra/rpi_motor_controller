4ef3c59956fc7f57224cd00ffae064ecbd1bc1ff
JordanKra <jkrasan@umich.edu>
2022-05-27 02:23:44 -0400

    Added steering basics and first attempt at steering based off angles

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ import max_10
 from time import sleep
 from machine import Pin, PWM
 def main():
-    esc = max_10.Max10(4, 50)
-    esc.arm_esc()
+    led = Pin(25, Pin.OUT)
+    # Max10(throttle control pin num, servo control pin num, freq)
+    esc = max_10.Max10(4, 5, 50)
+    #esc.arm_esc()
     while True:
-        esc.motor_test()
+        #esc.motor_test()
+        led.value(1)
+        esc.set_steering(0)
+        led.value(0)
+        sleep(2.0)
+        esc.set_steering(60)
+        led.value(1)
+        sleep(2.0)
+        esc.set_steering(180)
+        led.value(0)
+        sleep(2.0)
 
 if __name__ == '__main__':
     main()
--- max_10.py
+++ max_10.py
@@ -1,14 +1,27 @@
 # Class for controlling the Hobbywing EZRUN Max 10 ESC
 from time import sleep
 from machine import Pin, PWM
-MAX_DUTY = 65025
+MAX_DUTY = 65535
+
+
+def map_to_range(x, in_min, in_max, out_min, out_max):
+    #Python implementation of the arduino map function: https://www.arduino.cc/reference/en/language/functions/math/map/
+    return int((x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min)
+
 
 class Max10:
-    def __init__(self, pin, freq):
-        #Setup PWM
-        self.pwm = PWM(Pin(pin))
-        self.pwm.freq(freq)
+    def __init__(self, motor_pin, servo_pin, freq):
+        #Setup motor PWM
+        self.motor_pwm = PWM(Pin(motor_pin))
+        self.motor_pwm.freq(freq)
+
+        #Setup servo PWM
+        self.servo_pwm = PWM(Pin(servo_pin))
+        self.servo_pwm.freq(freq)
+
+        #Status LED pin setup
         self.led = Pin(25, Pin.OUT)
+
     def arm_esc(self):
         #According to to the docs this should just be neutral, full forward, then full reverse
         #Calculating the duty cycle is just doing   (desired_width / period) * 100%
@@ -20,28 +33,35 @@ class Max10:
         self.set_reverse(1.0)
         sleep(3.0)
 
-        return
     def set_netural(self):
         #set pin to netural
-        self.pwm.duty_u16(int((1.5/20.0) * 65535))
+        self.motor_pwm.duty_u16(int((1.5/20.0) * MAX_DUTY))
 
     def set_forward(self, percent):
         # set pin to a percentage of forward
-        self.pwm.duty_u16(int(((1.5 + (percent * 0.5)) / 20.0) * 65535))
+        self.motor_pwm.duty_u16(int(((1.5 + (percent * 0.5)) / 20.0) * MAX_DUTY))
 
     def set_reverse(self, percent):
         # set the pin to a percentage of reverse
-        self.pwm.duty_u16(int(((1.5 - (0.1 * 0.5)) / 20.0) * 65535))
+        self.motor_pwm.duty_u16(int(((1.5 - (0.1 * 0.5)) / 20.0) * MAX_DUTY))
         sleep(20*2/1000)
         self.set_netural()
-        sleep(20*4/1000)
-        self.pwm.duty_u16(int(((1.5 - (percent * 0.5)) / 20.0) * 65535))
+        sleep(1.0)
+        self.motor_pwm.duty_u16(int(((1.5 - (percent * 0.5)) / 20.0) * MAX_DUTY))
 
     def servo_test(self):
-        for duty in range(31512, 65025):
-            self.pwm.duty_u16(duty)
-            sleep(0.5)
-        self.pwm.duty_u16(31512)
+        self.servo_pwm.duty_u16(int((1.8/20.0) * MAX_DUTY))
+        sleep(2.0)
+        self.servo_pwm.duty_u16(int((1.0/20)*MAX_DUTY))
+        sleep(2.0)
+        self.servo_pwm.duty_u16(int((1.4/20.0) * MAX_DUTY))
+        sleep(2.0)
+
+    def set_steering(self, angle):
+        #Set servo position based on given angle
+        duty = int(map_to_range(angle, 0, 180, int(((1.0/20.0)*MAX_DUTY)), int(((2.0/20.0)*MAX_DUTY))))
+        self.servo_pwm.duty_u16(duty)
+
 
     def motor_test(self):
         self.led.value(1)
@@ -51,8 +71,5 @@ class Max10:
         sleep(2.0)
         self.set_reverse(0.10)
         sleep(2.0)
-        #self.pwm.duty_u16(65535)
-        #self.led.value(0)
-
 
 
